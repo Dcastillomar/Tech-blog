322e8eb9b285e1997c2dee98972860ffb3e071cc
Dcastillomar <125750537+Dcastillomar@users.noreply.github.com>
2023-06-23 11:59:39 -0500

    add server.js and utils folder

---
+++ server.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const expressHandlebars = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+
+//sequelize store using express-session
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = expressHandlebars.create({ helpers }); 
+
+//configure and link session object with sequelize store
+const sess = {
+    secret: 'Super secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizedStore({
+        db: sequelize
+    })
+};
+
+//middleware, add express-session
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
