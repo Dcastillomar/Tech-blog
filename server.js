893f151e13f3b89c9a4ef278bc614c2b4066109b
Dcastillomar <125750537+Dcastillomar@users.noreply.github.com>
2023-07-20 16:10:46 -0500

    more css changes

--- server.js
+++ server.js
@@ -52,6 +52,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('source'));
 
 
 app.use(routes);
